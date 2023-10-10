acbefa8a8f83eb20f924239b58c9f8627224e9d6
kevin Graff <kevin@kevin-graff.com>
2023-10-10 19:56:29 -0400

    finish just want to slow down rate of led

--- shed/shed.ino
+++ shed/shed.ino
@@ -7,7 +7,7 @@ char devicename[] = "shed";  //// needs to be the name of you give your device a
   EspMQTTClient client(
   "graffhouse",   // WIFI SSID
   "Kevin8386",    // WIFI Password
-  "192.168.18.185",  // MQTT Broker server ip
+  "192.168.18.161",  // MQTT Broker server ip
   "admin",   // Can be omitted if not needed
   "kevin8386",   // Can be omitted if not needed
   devicename,     // Client name that uniquely identify your device
@@ -21,10 +21,13 @@ const int Analogin = 36;
 const int Digout = 16;
 const int Bultinled = 2;
 int me;
+int batv[11];
+int count = 0;
 
 int battin;
 int acout;
 int ledout;
+int setout;
 
 
 
@@ -37,6 +40,7 @@ thismqtt.start(1,1,0,2);  //start the connection start(num of digital imputs, nu
 
 ////// each io is declared type of io (variable int, discription to be displayed)
 thismqtt.analoginput(&battin,"battery voltage");
+thismqtt.analogoutput(&setout,"AC power trip point");
 thismqtt.digitaloutput(&acout,"main 120vac power to shed");
 thismqtt.digitaloutput(&ledout,"on board led");
 thismqtt.run(&thismqtt, devicename); //// builds discriptor of the node infomation
@@ -57,21 +61,37 @@ void loop()
 
   // Execute delayed toggle of built  in led without slowing down loop
 if (mytime <= millis())    {
-//delay this part of the loop runs every 5 sec
+//delay this part of the loop runs every 3 sec
+count++;
+batv[count] = analogRead(Analogin);
+if (count == 10)
+{
+  count = 0;
+  battin = ((batv[1] + batv[2] + batv[3] + batv[4] + batv[5] + batv[6] + batv[7] + batv[8] + batv[9] + batv[10])/10 ) / 1.271;  //  get average of 10 readings and devide by trim amount
+  }
     if (me == 0)
         {
         ledout = 0;         ////////////    this just toggles the on board led
         mytime = millis();
-        mytime = mytime + 5000;
+        mytime = mytime + 3000;
         me = 100;
         }
     else {                                     // was here
         ledout = 100;
         mytime = millis();
-        mytime = mytime + 5000;
+        mytime = mytime + 3000;
         me = 0;
         }
 
+}
+if (setout >= battin )
+{
+      digitalWrite(Digout, LOW); 
+      acout = 0;
+}
+else {
+      digitalWrite(Digout, HIGH); 
+      acout = 100 ;
 }
 if (ledout == 0) {     
      digitalWrite(Bultinled, LOW);
